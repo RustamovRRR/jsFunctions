c441fc41559c1704207996860fdfb829d6cee47a
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-07 15:48:08 +0400

    working with IIFE

--- IIFE/app.js
+++ IIFE/app.js
@@ -4,3 +4,31 @@
 // good way at protecting the score of your function and the variables within it.
 
 // global namespace, extra steps
+
+const num1 = 20;
+const num2 = 30;
+
+function add() {
+  console.log(`Result: ${num1 + num2}`);
+}
+
+add();
+
+// IIFE
+(function () {
+  const num3 = 20;
+  const num4 = 30;
+  console.log(`Result with IIFE: ${num3 + num4}`);
+})();
+
+// pass arguments to IIFE
+(function (num1, num2) {
+  console.log(`Result with IIFE and argments: ${num1 + num2}`);
+})(15, 20);
+
+// assign function to a veriable
+const result = (function (num1, num2) {
+  return `Assign IIFE: ${num1 + num2}`;
+})(15, 20);
+
+console.log(result);
