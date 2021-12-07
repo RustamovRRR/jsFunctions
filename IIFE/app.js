d696fb0188adc2ec1d69f58906a2668c86fb43f6
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-07 15:33:59 +0400

    adding setup files for IIFE

---
+++ IIFE/app.js
@@ -0,0 +1,6 @@
+// IIFE - Imeddiately Invoked Function Expression
+// older approach - new modules
+// simple approach to avoid global scope pollution
+// good way at protecting the score of your function and the variables within it.
+
+// global namespace, extra steps
